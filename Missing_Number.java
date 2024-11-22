bdadecb29e4e1d39644a0e76c8012e3e2e258e26
Ruvaid Ahmad <118987852+Ruvaidahmad123@users.noreply.github.com>
2024-11-22 12:58:01 +0530

    Create Missing_Number.java

---
+++ Missing_Number.java
@@ -0,0 +1,74 @@
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.Math.abs;
+import static java.lang.System.out;
+import java.util.*;
+import java.io.*;
+import java.math.*;
+public class Missing_Number {
+    public static void main(String[] args) throws IOException {
+        FastReader in = new FastReader();
+        FastWriter out = new FastWriter();
+        long n=in.nextLong();
+        long sum=(n*(n+1))/2;
+        n=n-1;
+        while(n-->0){
+            int num=in.nextInt();
+            sum-=num;
+        }
+        System.out.print(sum);
+        out.close();
+    }
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine().trim();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+        //https://github.com/dr0nser/fast-io-java
+    static class FastWriter {
+        private final BufferedWriter bw;
+        public FastWriter() {
+            this.bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        }
+        public void print(Object object) throws IOException {
+            bw.append("" + object);
+        }
+        public void println(Object object) throws IOException {
+            print(object);
+            bw.append("\n");
+        }
+        public void close() throws IOException {
+            bw.close();
+        }
+    }
+}
